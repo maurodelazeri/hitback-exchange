4b6047edde56885057e0b95228b4f0ff6719b4cc
Mauro Delazeri <maurodelazeri@gmail.com>
2017-02-23 12:08:32 -0500

    usd currency

--- app/controllers/private/assets_controller.rb
+++ app/controllers/private/assets_controller.rb
@@ -4,15 +4,12 @@ module Private
 
     def index
       @cny_assets  = Currency.assets('cny')
-      @usd_assets  = Currency.assets('usd')
       @btc_proof   = Proof.current :btc
       @cny_proof   = Proof.current :cny
-      @usd_proof   = Proof.current :usd
 
       if current_user
         @btc_account = current_user.accounts.with_currency(:btc).first
         @cny_account = current_user.accounts.with_currency(:cny).first
-        @usd_account = current_user.accounts.with_currency(:usd).first
       end
     end
 
